dabbf4158780a9e64e51241223653915a20d9065
dinh-code <63176375+dinh-code@users.noreply.github.com>
2023-05-03 23:06:34 +0700

    fix

--- push.js
+++ push.js
@@ -1,20 +1,21 @@
 var s = new URL(location.href).searchParams.get("bill");
 var j = JSON.parse(decodeURIComponent(s));
+var qrSize = 150;
 
 document.title="["+j.key+"]Nha khoa SV - Hóa đơn";
 //Khách hàng
 document.getElementById("hoTen").innerText=j.hoTen;
 document.getElementById("sdt").innerText=j.sdt;
-document.getElementById("diaChi").innerText=j.diaChi;
+document.getElementById("diaChi").innerText="Địa chỉ: "+j.diaChi;
 //Thông tin đơn hàng
 document.getElementById("key").innerText=j.key;
 document.getElementById("ngayDat").innerText=j.ngayDat;
 document.getElementById("tenDVVC").innerText=j.tenDVVC;
 document.getElementById("maDon").innerText=j.maDon;
-document.getElementById("keyQr").src="https://chart.googleapis.com/chart?chs=150x150&cht=qr&chl=https%3A%2F%2Fwww.nhakhoasv.com%2F%3ForderKey%3D"+j.key;
-//document.getElementById("momo").src="https://chart.googleapis.com/chart?chs=150x150&cht=qr&chl=2%7C99%7C0968747831%7CDinh%20Phuoc%20An%7C%7C0%7C0%7C%7Cho ten va sdt%7Ctransfer_myqr";
-document.getElementById("momo").src="https://chart.googleapis.com/chart?chs=150x150&cht=qr&chl=2%7C99%7C0968747831%7CDinh%20Phuoc%20An%7C%7C0%7C0%7C"+j.tienHangN+"%7C"+j.key+"%20"+j.hoTen+"%20"+j.sdt+"%7Ctransfer_myqr";
-document.getElementById("vcb").src="https://chart.googleapis.com/chart?chs=120x120&cht=qr&chl=00020101021138540010A00000072701240006970436011099687478310208QRIBFTTA53037045802VN62170813ho ten va sdt63046D75";
+new QRCode(document.getElementById("keyQr"), {text: "https://nhakhoasv.com?orderKey="+j.key, width: qrSize, height: qrSize});
+new QRCode(document.getElementById("momo"), {text: "2|99|0968747831|Dinh Phuoc An||0|0|"+j.tienHangN+"|"+j.key+" "+removeVietnameseTones(j.hoTen)+" "+j.sdt+"|transfer_myqr", width: qrSize, height: qrSize});
+new QRCode(document.getElementById("gmap"), {text: "https://goo.gl/maps/WNVStknA7ZxCAQG48", width: qrSize, height: qrSize});
+document.getElementById("vcb").src="https://img.vietqr.io/image/VCB-9968747831-qr_only.png?amount="+j.tienHangN+"&addInfo="+j.key+"%20"+j.hoTen+"%20"+j.sdt+"&accountName=Dinh phuoc an";
 //Chi tiết
 j.chiTiet.sort((a, b)=>{
     if(a.sku > b.sku) return -1;
